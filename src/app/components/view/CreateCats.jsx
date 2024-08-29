73f6606d60c557797197aa478b3bf70031533dff
Cristianyelmo <cristianama2019@gmail.com>
2024-08-29 10:50:32 -0300

    view create test

--- src/app/components/view/CreateCats.jsx
+++ src/app/components/view/CreateCats.jsx
@@ -41,52 +41,79 @@ if(!loadingcreate){
   
 
   return (
-    <div className="flex fondo-content">
-      <button onClick={Volver}>volver</button>
+    <div className="flex flex-col lg:flex-row fondo-content justify-center items-center min-h-screen ">
+       <button onClick={Volver}>volver</button>
+      <div className="flex flex-col items-center">
+   
+   <Modelss  texture={selectedValue} editandcreate={true} />
+
+  
+
+ <input
+ className="w-[300px] neon-border-input mt-4 p-2"
+ 
+     onChange={(e) => {
+       setSelectedValuetext(e.target.value);
+     }}
+     type="text"
+     value={selectedValuetext}
+   />
+   </div>
+
+<div className="space-y-4 ml-10 mt-10 lg:mt-0 ">
+
+      <div>
+<p className="text-[#fac81c]">Color</p>
       <select
         value={selectedValue[0].name}
+         className="neon-border-ver"
         onChange={(e) => {
           handleUpdate(e, "Cube002");
           changeTexture(e.target.value, "Cube002");
         }}
       >
-        <option value="dfd.png">Option 1</option>
-        <option value="dfd7.png">Option 2</option>
-        <option value="dfd8.png">Option 2</option>
-        <option value="dfd9.png">Option 2</option>
-        <option value="dfd10.png">Option 2</option>
-        <option value="dfd11.png">Option 2</option>
-        <option value="dfd12.png">Option 2</option>
-        <option value="dfd13.png">Option 2</option>
-        <option value="dfd14.png">Option 2</option>
-        <option value="dfd15.png">Option 2</option>
-        <option value="dfd16.png">Option 2</option>
-        <option value="dfd17.png">Option 2</option>
+        <option value="dfd.png">Verde</option>
+        <option value="dfd7.png">Violeta</option>
+        <option value="dfd8.png">Deadpool</option>
+        <option value="dfd9.png">Rorschach</option>
+        <option value="dfd10.png">Naranja</option>
+        <option value="dfd11.png">Rosa</option>
+        <option value="dfd12.png">Spawn</option>
+        <option value="dfd13.png">Pikachu</option>
+        <option value="dfd14.png">Joker</option>
+        <option value="dfd15.png">Sans</option>
+        <option value="dfd16.png">Eva-01</option>
+        <option value="dfd17.png">Luna</option>
       </select>
+      </div>
 
 
-
-
+<div>
+<p className="text-[#dd0303]">Anteojos</p>
       <select
         value={selectedValue[1].name}
+         className="neon-border2"
         onChange={(e) => {
           handleUpdate(e, "Cube001");
           changeTexture(e.target.value, "Cube001");
         }}
       >
-        <option value="Proyecto nuevocvc.png">Option 1</option>
-        <option value="Proyecto nuevocvc1.png">Option 2</option>
-        <option value="Proyecto nuevocvc2.png">Option 2</option>
-        <option value="Proyecto nuevocvc3.png">Option 2</option>
-        <option value="Proyecto nuevocvc4.png">Option 2</option>
-        <option value="Proyecto nuevocvc5.png">Option 2</option>
-        <option value="Proyecto nuevocvc6.png">Option 2</option>
-        <option value="Proyecto nuevocvc7.png">Option 2</option>
+        <option value="Proyecto nuevocvc.png">Harry Potter</option>
+        <option value="Proyecto nuevocvc1.png">Rojos</option>
+        <option value="Proyecto nuevocvc2.png">Negros</option>
+        <option value="Proyecto nuevocvc3.png">3D</option>
+        <option value="Proyecto nuevocvc4.png">Ciclope</option>
+        <option value="Proyecto nuevocvc5.png">Vaporwave</option>
+        <option value="Proyecto nuevocvc6.png">Corazones</option>
+        <option value="Proyecto nuevocvc7.png">Naranja</option>
         <option value="nada">Nada</option>
       </select>
-
+      </div>
+<div>
+  <p className="text-[#1774ff]">Ropa</p>
       <select
         value={selectedValue[2].name}
+         className="neon-border-editar"
         onChange={(e) => {
           handleUpdate(e, "Cube003");
           changeTexture(e.target.value, "Cube003");
@@ -97,25 +124,22 @@ if(!loadingcreate){
         <option value="dfd4.png">Boca</option>
         <option value="dfd5.png">Punisher</option>
         <option value="dfd6.png">Trajecito</option>
-        <option value="dfd18.png">Trajecito</option>
-        <option value="dfd19.png">Trajecito</option>
-        <option value="dfd20.png">Trajecito</option>
+        <option value="dfd18.png">Nirvana</option>
+        <option value="dfd19.png">Barbie</option>
+        <option value="dfd20.png">Scarface</option>
         <option value="nada">Nada</option>
       </select>
+      </div>
+     
+      <button className="neon-border-crear p-4 mt-10" onClick={CreatedArray}>crear</button>
 
-      <div>
-        <Modelss  texture={selectedValue} editandcreate={true} />
-
-        <input
-          onChange={(e) => {
-            setSelectedValuetext(e.target.value);
-          }}
-          type="text"
-          value={selectedValuetext}
-        />
       </div>
 
-      <button onClick={CreatedArray}>crear</button>
+     
+
+
+       
+      
     </div>
   );
 }
