4b6c1c90de8d43b77c36172da3f8a2fdfef445ae
Cristianyelmo <cristianama2019@gmail.com>
2024-08-30 16:15:52 -0300

    test icon 2

--- src/app/components/view/Cats.jsx
+++ src/app/components/view/Cats.jsx
@@ -27,39 +27,43 @@ export default function Cats() {
     setDelete,
     addscore,
     scoreid,
-    visiblePages
+    visiblePages,
   } = MichiHook();
-const {resetSelectedValue} =MichiCreateAndEditedHook()
+  const { resetSelectedValue } = MichiCreateAndEditedHook();
   useEffect(() => {
     GetallMichis();
-    console.log(addscore)
+    console.log(addscore);
   }, []);
   const updateNew = () => {
     setChangepage("CreateCats");
     resetSelectedValue();
   };
-console.log(currentGatos)
+  console.log(currentGatos);
   if (loading) {
-    return <div className="bg-black text-white relative h-screen w-full  fondo-content">
-      <div className="absolute inset-0 flex flex-col justify-center items-center">
-    <p className="hollow-neon-text text-3xl mb-4">cargando...</p>
-    <Image src="/texture/tte.png" width={128} height={128} alt="Cargando" />
-  </div>
-      </div>;
-        
-  } 
-
-
-
     return (
-      <div className="flex fondo-content  justify-center items-center min-h-screen mt-10">
-        {modal.delete && (
-          <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-80">
-            <div class="neon-border p-6 rounded-lg shadow-lg w-80">
-              <h2 class="text-xl font-bold mb-4">
-                quieres eliminar a {infoModal.name} ?
-              </h2>
-              <div className="space-x-2">
+      <div className="bg-black text-white relative h-screen w-full  fondo-content">
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
+          <p className="hollow-neon-text text-3xl mb-4">cargando...</p>
+          <Image
+            src="/texture/tte.png"
+            width={128}
+            height={128}
+            alt="Cargando"
+          />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex fondo-content  justify-center items-center min-h-screen mt-10">
+      {modal.delete && (
+        <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-80">
+          <div class="neon-border p-6 rounded-lg shadow-lg w-80">
+            <h2 class="text-xl font-bold mb-4">
+              quieres eliminar a {infoModal.name} ?
+            </h2>
+            <div className="space-x-2">
               <button
                 class=" text-white py-2 px-4 rounded neon-border2 "
                 onClick={() => gatosDelete(infoModal.id)}
@@ -68,161 +72,150 @@ console.log(currentGatos)
               </button>
               <button
                 class="neon-border-ver text-white py-2 px-4 rounded "
-                onClick={() => setModal(prevState => ({
-                  ...prevState,
-                  delete: false
-                }))}
+                onClick={() =>
+                  setModal((prevState) => ({
+                    ...prevState,
+                    delete: false,
+                  }))
+                }
               >
                 cerrar
               </button>
-              </div>
             </div>
           </div>
-        )}
+        </div>
+      )}
 
-        {modal.view && (
-          <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-80">
-            <div class=" p-6 rounded-lg shadow-lg bg-transparent">
-              <button onClick={() => setModal(prevState => ({
+      {modal.view && (
+        <div class="fixed inset-0 z-50 flex items-center justify-center bg-gray-900 bg-opacity-80">
+          <div class=" p-6 rounded-lg shadow-lg bg-transparent">
+            <button
+              onClick={() =>
+                setModal((prevState) => ({
                   ...prevState,
-                  view: false
-                }))}>cerrar</button>
-              <Modelss
-                texture={infoModal.textura}
-                editandcreate={false}
-                orbitControls={true}
-              />
-            </div>
+                  view: false,
+                }))
+              }
+            >
+              cerrar
+            </button>
+            <Modelss
+              texture={infoModal.textura}
+              editandcreate={false}
+              orbitControls={true}
+            />
           </div>
-        )}
+        </div>
+      )}
 
-
-
-{deletex && (
-          <div
-            className={`fixed inset-0 z-40 flex items-center justify-center bg-gray-900 bg-opacity-50 
+      {deletex && (
+        <div
+          className={`fixed inset-0 z-40 flex items-center justify-center bg-gray-900 bg-opacity-50 
   transition-opacity duration-500 ease-in-out ${
     deletex ? "opacity-100" : "opacity-0"
   }`}
-          ></div>
-        )}
-      
-        <div className="flex flex-col items-center">
+        ></div>
+      )}
+
+      <div className="flex flex-col items-center">
         <h1 className="neon-text text-4xl  ">PetMaker</h1>
         <div className="relative mb-14 w-full">
-  <button
-    className="neon-border-crear p-2 absolute right-0"
-    onClick={updateNew}
-  >
-    crear
-  </button>
-</div>
-
-     
-          <div className="grid lg:grid-cols-3 lg:gap-3 grid-cols-1 gap-1 justify-center lg:space-y-0 space-y-10">
-            {currentGatos.map((gato, index) => (
-              <div
-                key={gato.id}
-                className={` ${
-                  idanimation == gato.id ? (setDelete(true), "animation-delete z-50"): "animation-open"
-                }`}
-              >
-                  <Modelss
-                  orbitControls={false}
-                  texture={gato.textura}
-                  editandcreate={false}
-                  
-                />  
-                {/* <div className={`w-[300px] h-[300px] `}>
-
-                </div> */}
-                
-               
-                <p className="text-center text-2xl">{gato.name}</p>
-               <div className="text-white flex justify-center space-x-2 mb-2">
-                <p className="text-2xl">{gato.puntaje}</p>
-        
-                <button disabled={addscore} onClick={() => AddScore(gato.id)}>
-                <Image className={`${scoreid == gato.id && addscore && 'rotate-start'}`} src="/texture/star.webp" width={30} height={30} />
-                </button>
-                 
-                </div>
-                <div className="space-x-2">
-                  <button
-                    onClick={() => gatofind(gato.id)}
-                    className="bg-black text-white p-2 neon-border-editar"
-                  >
-                    editar
-                  </button>
-                  <button
-                    onClick={() => gatoModal(gato.id,'delete')}
-                    className="bg-transparent text-white p-2 neon-border2"
-                  >
-                    eliminar
-                  </button>
-                  <button
-                    onClick={() => gatoModal(gato.id,'view')}
-                    className="bg-black text-white p-2 neon-border-ver"
-                  >
-                    ver
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-         
-         
-       
-          <div className="flex items-center justify-center mt-10 mb-10  ">
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-          className="px-3 py-1 neon-border  rounded-l disabled:opacity-50"
-        >
-          &lt;
-        </button>
-
-        {visiblePages.map((page) => (
           <button
-            key={page}
-            onClick={() => handlePageChange(page)}
-            className={`px-3 py-1 border-[#24c2d3] border-[1px]  ${page === currentPage ? 'bg-[#3b094d] text-white' : ''}`}
+            className="neon-border-crear p-2 absolute right-0"
+            onClick={updateNew}
           >
-            {page}
+            crear
           </button>
-        ))}
-
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
-          className="px-3 py-1 neon-border  rounded-r disabled:opacity-50"
-        >
-          &gt;
-        </button>
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         </div>
 
-   
-       
+        <div className="grid lg:grid-cols-3 lg:gap-3 grid-cols-1 gap-1 justify-center lg:space-y-0 space-y-10">
+          {currentGatos.map((gato, index) => (
+            <div
+              key={gato.id}
+              className={` ${
+                idanimation == gato.id
+                  ? (setDelete(true), "animation-delete z-50")
+                  : "animation-open"
+              }`}
+            >
+              <Modelss
+                orbitControls={false}
+                texture={gato.textura}
+                editandcreate={false}
+              />
+              {/* <div className={`w-[300px] h-[300px] `}>
+
+                </div> */}
+
+              <p className="text-center text-2xl">{gato.name}</p>
+              <div className="text-white flex justify-center space-x-2 mb-2">
+                <p className="text-2xl">{gato.puntaje}</p>
+
+                <button disabled={addscore} onClick={() => AddScore(gato.id)}>
+                  <Image
+                    className={`${
+                      scoreid == gato.id && addscore && "rotate-start"
+                    }`}
+                    src="/texture/star.webp"
+                    width={30}
+                    height={30}
+                  />
+                </button>
+              </div>
+              <div className="space-x-2">
+                <button
+                  onClick={() => gatofind(gato.id)}
+                  className="bg-black text-white p-2 neon-border-editar"
+                >
+                  editar
+                </button>
+                <button
+                  onClick={() => gatoModal(gato.id, "delete")}
+                  className="bg-transparent text-white p-2 neon-border2"
+                >
+                  eliminar
+                </button>
+                <button
+                  onClick={() => gatoModal(gato.id, "view")}
+                  className="bg-black text-white p-2 neon-border-ver"
+                >
+                  ver
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="flex items-center justify-center mt-10 mb-10  ">
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+            className="px-3 py-1 neon-border  rounded-l disabled:opacity-50"
+          >
+            &lt;
+          </button>
+
+          {visiblePages.map((page) => (
+            <button
+              key={page}
+              onClick={() => handlePageChange(page)}
+              className={`px-3 py-1 border-[#24c2d3] border-[1px]  ${
+                page === currentPage ? "bg-[#3b094d] text-white" : ""
+              }`}
+            >
+              {page}
+            </button>
+          ))}
+
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+            className="px-3 py-1 neon-border  rounded-r disabled:opacity-50"
+          >
+            &gt;
+          </button>
+        </div>
       </div>
-    );
- 
+    </div>
+  );
 }
--- src/app/components/view/CreateCats.jsx
+++ src/app/components/view/CreateCats.jsx
@@ -7,152 +7,155 @@ import { MichiCreateAndEditedHook } from "@/app/context/MichiCreateAndEditedCont
 import Image from "next/image";
 
 export default function CreateCats() {
- const 
-  {
-    CreatedArray,selectedValuetext, setSelectedValuetext,
+  const {
+    CreatedArray,
+    selectedValuetext,
+    setSelectedValuetext,
     selectedValue,
-    changeTexture,resetSelectedValue,handleUpdate,Volver,loadingcreate,setLoadingCreate,
-    errorMessage,createandediteloading
-  }
-  = MichiCreateAndEditedHook()
+    changeTexture,
+    resetSelectedValue,
+    handleUpdate,
+    Volver,
+    loadingcreate,
+    setLoadingCreate,
+    errorMessage,
+    createandediteloading,
+  } = MichiCreateAndEditedHook();
 
- const {changepage} = MichiHook()
-  
- useEffect(() => {
-  if (changepage == 'CreateCats') {
-    
-    setLoadingCreate(true)
-    resetSelectedValue();
-  }
-}, [changepage]);
+  const { changepage } = MichiHook();
 
- 
- 
-if(!loadingcreate){
-  return <div className="bg-black text-white relative h-screen w-full  fondo-content">
-  <div className="absolute inset-0 flex flex-col justify-center items-center">
-<p className="hollow-neon-text text-3xl mb-4">cargando...</p>
-<Image src="/texture/tte.png" width={128} height={128} alt="Cargando" />
-</div>
-  </div>;
-}else{
+  useEffect(() => {
+    if (changepage == "CreateCats") {
+      setLoadingCreate(true);
+      resetSelectedValue();
+    }
+  }, [changepage]);
 
-  
-  
-
-  return (
-    <div className="flex flex-col lg:flex-row fondo-content justify-center items-center min-h-screen ">
-
-       { createandediteloading &&  <div
+  if (!loadingcreate) {
+    return (
+      <div className="bg-black text-white relative h-screen w-full  fondo-content">
+        <div className="absolute inset-0 flex flex-col justify-center items-center">
+          <p className="hollow-neon-text text-3xl mb-4">cargando...</p>
+          <Image
+            src="/texture/tte.png"
+            width={128}
+            height={128}
+            alt="Cargando"
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col lg:flex-row fondo-content justify-center items-center min-h-screen ">
+        {createandediteloading && (
+          <div
             className={`fixed inset-0 z-40 flex items-center justify-center bg-gray-900 bg-opacity-70 
   transition-opacity duration-500 ease-in-out`}
-          ></div>}
-        
+          ></div>
+        )}
 
+        <button className="neon-border-volver p-4 mr-4 mb-4" onClick={Volver}>
+          volver
+        </button>
+        <div className="animation-open z-50 flex flex-col items-center">
+          <Modelss texture={selectedValue} editandcreate={true} />
 
-       <button className="neon-border-volver p-4 mr-4 mb-4" onClick={Volver}>volver</button>
-      <div className="animation-open z-50 flex flex-col items-center">
-   
-   <Modelss  texture={selectedValue} editandcreate={true} />
+          <input
+            className="w-[300px] neon-border-input mt-4 p-2"
+            readOnly={createandediteloading}
+            onChange={(e) => {
+              setSelectedValuetext(e.target.value);
+            }}
+            type="text"
+            value={selectedValuetext}
+          />
+        </div>
+        <div className="flex flex-col items-center lg:items-start">
+          <div className="space-y-4 ml-10 mt-2 lg:mt-0 ">
+            <div
+              className={`relative mb-10  ${errorMessage == "" && "hidden"}`}
+            >
+              <p className=" text-[#dd0303] absolute">{errorMessage}</p>
+            </div>
+            <div>
+              <p className="text-[#fac81c]">Color</p>
+              <select
+                value={selectedValue[0].name}
+                className="neon-border-ver"
+                onChange={(e) => {
+                  handleUpdate(e, "Cube002");
+                  changeTexture(e.target.value, "Cube002");
+                }}
+              >
+                <option value="dfd.png">Verde</option>
+                <option value="dfd7.png">Violeta</option>
+                <option value="dfd8.png">Deadpool</option>
+                <option value="dfd9.png">Rorschach</option>
+                <option value="dfd10.png">Naranja</option>
+                <option value="dfd11.png">Rosa</option>
+                <option value="dfd12.png">Spawn</option>
+                <option value="dfd13.png">Pikachu</option>
+                <option value="dfd14.png">Joker</option>
+                <option value="dfd15.png">Sans</option>
+                <option value="dfd16.png">Eva-01</option>
+                <option value="dfd17.png">Luna</option>
+              </select>
+            </div>
 
-  
+            <div>
+              <p className="text-[#dd0303]">Anteojos</p>
+              <select
+                value={selectedValue[1].name}
+                className="neon-border2"
+                onChange={(e) => {
+                  handleUpdate(e, "Cube001");
+                  changeTexture(e.target.value, "Cube001");
+                }}
+              >
+                <option value="Proyecto nuevocvc.png">Harry Potter</option>
+                <option value="Proyecto nuevocvc1.png">Rojos</option>
+                <option value="Proyecto nuevocvc2.png">Negros</option>
+                <option value="Proyecto nuevocvc3.png">3D</option>
+                <option value="Proyecto nuevocvc4.png">Ciclope</option>
+                <option value="Proyecto nuevocvc5.png">Vaporwave</option>
+                <option value="Proyecto nuevocvc6.png">Corazones</option>
+                <option value="Proyecto nuevocvc7.png">Naranja</option>
+                <option value="nada">Nada</option>
+              </select>
+            </div>
+            <div>
+              <p className="text-[#1774ff]">Ropa</p>
+              <select
+                value={selectedValue[2].name}
+                className="neon-border-editar"
+                onChange={(e) => {
+                  handleUpdate(e, "Cube003");
+                  changeTexture(e.target.value, "Cube003");
+                }}
+              >
+                <option value="dfd2.png">Argentina</option>
+                <option value="dfd3.png">River</option>
+                <option value="dfd4.png">Boca</option>
+                <option value="dfd5.png">Punisher</option>
+                <option value="dfd6.png">Trajecito</option>
+                <option value="dfd18.png">Nirvana</option>
+                <option value="dfd19.png">Barbie</option>
+                <option value="dfd20.png">Scarface</option>
+                <option value="nada">Nada</option>
+              </select>
+            </div>
 
- <input
- className="w-[300px] neon-border-input mt-4 p-2"
- readOnly={createandediteloading}
-     onChange={(e) => {
-       setSelectedValuetext(e.target.value);
-     }}
-     type="text"
-     value={selectedValuetext}
-   />
-   </div>
-<div className="flex flex-col items-center lg:items-start">
-<div className="space-y-4 ml-10 mt-2 lg:mt-0 ">
-<div className={`relative mb-10  ${errorMessage == '' && 'hidden'}`}>
-<p className=" text-[#dd0303] absolute">{errorMessage}</p>
-</div>
-      <div >
-<p className="text-[#fac81c]">Color</p>
-      <select
-        value={selectedValue[0].name}
-         className="neon-border-ver"
-        onChange={(e) => {
-          handleUpdate(e, "Cube002");
-          changeTexture(e.target.value, "Cube002");
-        }}
-      >
-        <option value="dfd.png">Verde</option>
-        <option value="dfd7.png">Violeta</option>
-        <option value="dfd8.png">Deadpool</option>
-        <option value="dfd9.png">Rorschach</option>
-        <option value="dfd10.png">Naranja</option>
-        <option value="dfd11.png">Rosa</option>
-        <option value="dfd12.png">Spawn</option>
-        <option value="dfd13.png">Pikachu</option>
-        <option value="dfd14.png">Joker</option>
-        <option value="dfd15.png">Sans</option>
-        <option value="dfd16.png">Eva-01</option>
-        <option value="dfd17.png">Luna</option>
-      </select>
+            <button
+              className="neon-border-crear p-4 mt-10"
+              onClick={CreatedArray}
+            >
+              crear
+            </button>
+            <div></div>
+          </div>
+        </div>
       </div>
-
-
-<div>
-<p className="text-[#dd0303]">Anteojos</p>
-      <select
-        value={selectedValue[1].name}
-         className="neon-border2"
-        onChange={(e) => {
-          handleUpdate(e, "Cube001");
-          changeTexture(e.target.value, "Cube001");
-        }}
-      >
-        <option value="Proyecto nuevocvc.png">Harry Potter</option>
-        <option value="Proyecto nuevocvc1.png">Rojos</option>
-        <option value="Proyecto nuevocvc2.png">Negros</option>
-        <option value="Proyecto nuevocvc3.png">3D</option>
-        <option value="Proyecto nuevocvc4.png">Ciclope</option>
-        <option value="Proyecto nuevocvc5.png">Vaporwave</option>
-        <option value="Proyecto nuevocvc6.png">Corazones</option>
-        <option value="Proyecto nuevocvc7.png">Naranja</option>
-        <option value="nada">Nada</option>
-      </select>
-      </div>
-<div>
-  <p className="text-[#1774ff]">Ropa</p>
-      <select
-        value={selectedValue[2].name}
-         className="neon-border-editar"
-        onChange={(e) => {
-          handleUpdate(e, "Cube003");
-          changeTexture(e.target.value, "Cube003");
-        }}
-      >
-        <option value="dfd2.png">Argentina</option>
-        <option value="dfd3.png">River</option>
-        <option value="dfd4.png">Boca</option>
-        <option value="dfd5.png">Punisher</option>
-        <option value="dfd6.png">Trajecito</option>
-        <option value="dfd18.png">Nirvana</option>
-        <option value="dfd19.png">Barbie</option>
-        <option value="dfd20.png">Scarface</option>
-        <option value="nada">Nada</option>
-      </select>
-      </div>
-     
-      <button className="neon-border-crear p-4 mt-10" onClick={CreatedArray}>crear</button>
-<div>
-  
-      </div>
-     
-      </div>
-
-      </div>
-
-     
-      
-    </div>
-  );
-}
+    );
+  }
 }
